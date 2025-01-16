6fcafa38569d0f32ddbc4795c40c839f3630f085
BClouden64 <brandonclouden@gmail.com>
2025-01-16 11:00:27 -0500

    why in Github

---
+++ school/hardware_software_code/BC_project_01.py
@@ -0,0 +1,14 @@
+def main():
+    print("Welcome! This program will ask you a few questions and summarize your answers.")
+    print()
+    name = input("What is your name? ")
+    college = input("What college do you attend? ")
+    high_school = input("What high school did you attend? ")
+    print("\nThank you for your responses!")
+    print(f"Your name is Brandon.")
+    print(f"You attend Guttman Community College.")
+    print(f"You went to high school at Tiegerman High School.")
+    print("Goodbye and have a great day!")
+
+if __name__ == "__main__":
+    main()
