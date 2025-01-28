d20eb8d47f5f752e0a002f4c8de4e326998484b1
BClouden64 <brandonclouden@gmail.com>
2025-01-28 10:57:07 -0500

    1/28/2025 H&S Class Work

--- school/hardware_software_code/BC_project_01.py
+++ school/hardware_software_code/BC_project_01.py
@@ -1,14 +1,23 @@
 def main():
-    print("Welcome! This program will ask you a few questions and summarize your answers.")
-    print()
-    name = input("What is your name? ")
-    college = input("What college do you attend? ")
-    high_school = input("What high school did you attend? ")
-    print("\nThank you for your responses!")
-    print(f"Your name is Brandon.")
-    print(f"You attend Guttman Community College.")
-    print(f"You went to high school at Tiegerman High School.")
-    print("Goodbye and have a great day!")
+    print("Hello, I would like to get to know a little about you.")
+    print("Please answer a few brief questions.")
+
+    name = "Brandon"
+    college = "Guttman CC"
+    high_school = "Tiegerman High School"
+    more_fun = college
+
+    print(f"What is your name?{name}")
+    print(f"{name}, What college are you attending?{college}")
+    print(f"{name}, What high school did you attend?{high_school}")
+    print(f"{name}, Which institution is more fun?{more_fun}")
+
+    print(f"\nIt was nice to speak with you, {name}")
+    print(f"You are currently attending, {college}")
+    print(f"I learned that your high school's name is {high_school}")
+    print(f"You think {college} is more fun than {high_school}")
+    print("It was fun getting to know a little about you.")
+    print("Let's do this again!")
 
 if __name__ == "__main__":
     main()
